9aa78d8c579a0fdbe70429a938fdf5f7519c1177
dhdc1 <dhdctraining@gmail.com>
2019-05-05 22:25:22 +0700

    sql

--- app.py
+++ app.py
@@ -104,6 +104,9 @@ def regis():
         line_id = request.form['line_id']
         print(cid, byear, line_id)
 
+        sql = "insert into line (cid,line_id,d_update) values ( '{0}' , '{1}' ,CURRENT_DATE()) "
+
+
         return render_template('ok.html')
 
 
